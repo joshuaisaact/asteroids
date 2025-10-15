1225ccf7aef4448fec88d025c0b4367146d311c4
Joshua Tuddenham <joshuaisaact@gmail.com>
2025-10-15 07:03:06 +0100

    feat: sprite grouping

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,10 +20,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS

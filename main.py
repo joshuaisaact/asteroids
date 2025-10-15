43b65b26963a9b0864ce015cfb7a94967f63d67b
Joshua Tuddenham <joshuaisaact@gmail.com>
2025-10-15 06:57:55 +0100

    feat: player forward and back

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,12 +20,10 @@ def main():
         player.draw(screen)
         pygame.display.flip()
 
-
-    # limit the framerate to 60 FPS
-    dt = clock.tick(60) / 1000
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+

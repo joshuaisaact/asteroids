18e63761d01d126ccb746ae63bfdddd376328c03
Joshua Tuddenham <joshuaisaact@gmail.com>
2025-10-15 07:23:46 +0100

    feat: collision

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -28,6 +29,11 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
